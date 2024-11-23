e8d30ccf219cce88a6523baaa254ef35ee46b904
Luke Sawczak <luke@sawczak.com>
2024-11-23 14:38:43 -0500

    fixes

--- src/tinypng.py
+++ src/tinypng.py
@@ -1,7 +1,6 @@
 # https://tinypng.com/developers/reference/python
 
 import tinify
-import os, time
 from pathlib import Path
 from progressbar import ProgressBar as pb
 
@@ -19,19 +18,28 @@ if not PATH_INPUT.exists():
 with open(Path(PATH_KEY), 'r') as f:
     tinify.key = f.read().strip()
 
-names = list(path.name for path in Path('src/input').glob('*.*'))
-if not names:
-    print('No files found in input folder')
+paths = list(path for path in Path('src/input').glob('*.*'))
+if not paths:
+    print('No files found in input folder. Exiting')
     exit()
+
+convert = input('Convert to webp? [Y/N; default N]: ').upper().strip()
+convert = convert.startswith('Y')
 
 n_success = 0
 n_failure = 0
 
-for name in pb()(names):
+for path in pb()(paths):
 
     try:
-        source = tinify.from_file(PATH_INPUT / name)
-        source.to_file(PATH_OUTPUT / name)
+        source = tinify.from_file(PATH_INPUT / path.name)
+        
+        if convert:
+            source = source.convert(type='image/webp')
+            source.to_file(PATH_OUTPUT / f'{path.stem}.webp')
+
+        else:
+            source.to_file(PATH_OUTPUT / path.name)
 
         n_success += 1
 
@@ -39,9 +47,9 @@ for name in pb()(names):
         n_failure += 1
 
         with open(PATH_DEBUG, 'a') as f:
-            f.write(str(e))
-            f.write('\n')
+            f.write(f'{path}\n{e}\n\n')
 
-print(f'Converted {n_success} / {len(names)} ({n_success / len(names) * 100:.2f})%')
+word = 'Shrunk' if not convert else 'Converted'
+print(f'{word} {n_success} / {len(paths)} ({n_success / len(paths) * 100:.2f})%')
 if n_failure:
     print(f'Consult {PATH_DEBUG} for error log')
